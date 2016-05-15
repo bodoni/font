8a1316dfa9a1874f0c5b1ec9c7b258ed2810d344
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-05-15 12:58:09 +0200

    Start to draw glyphs

--- src/format/opentype/case.rs
+++ src/format/opentype/case.rs
@@ -3,7 +3,7 @@ use super::postscript::compact::FontSet;
 use super::postscript::type2::Program;
 
 use Result;
-use glyph::Glyph;
+use glyph::{Builder, Glyph};
 use super::mapping::Mapping;
 
 pub struct PostScript {
@@ -21,13 +21,111 @@ impl PostScript {
 
 impl ::case::Case for PostScript {
     fn draw(&self, glyph: char) -> Result<Option<Glyph>> {
-        let id = match self.mapping.find(glyph) {
-            Some(id) => id,
-            _ => return Ok(None),
+        use super::postscript::type2::Operator::*;
+
+        let mut program = {
+            let id = match self.mapping.find(glyph) {
+                Some(id) => id,
+                _ => return Ok(None),
+            };
+            Program::new(&self.fontset.char_strings[self.id][id],
+                         &self.fontset.global_subroutines,
+                         &self.fontset.local_subroutines[self.id])
         };
-        let _ = Program::new(&self.fontset.char_strings[self.id][id],
-                             &self.fontset.global_subroutines,
-                             &self.fontset.local_subroutines[self.id]);
-        Ok(None)
+
+        let mut builder = Builder::new();
+
+        let mut clear = false;
+        while let Some((operator, operands)) = try!(program.next()) {
+            let count = operands.len();
+            match operator {
+                RMoveTo => {
+                    assert!(count == 2 || !clear && count == 3);
+                    builder.move_to(operands[0].into(), operands[1].into());
+                },
+                HMoveTo => {
+                    assert!(count == 1 || !clear && count == 2);
+                    builder.move_to(operands[0].into(), 0.0);
+                },
+                VMoveTo => {
+                    assert!(count == 1 || !clear && count == 2);
+                    builder.move_to(0.0, operands[0].into());
+                },
+                RLineTo => {
+                    assert!(count % 2 == 0);
+                    for i in 0..(count >> 1) {
+                        builder.line_to(operands[2 * i + 0].into(),
+                                        operands[2 * i + 1].into());
+                    }
+                },
+                HLineTo => {
+                    for i in 0..count {
+                        if i % 2 == 0 {
+                            builder.line_to(operands[i].into(), 0.0);
+                        } else {
+                            builder.line_to(0.0, operands[i].into());
+                        }
+                    }
+                },
+                VLineTo => {
+                    for i in 0..count {
+                        if i % 2 == 0 {
+                            builder.line_to(0.0, operands[i].into());
+                        } else {
+                            builder.line_to(operands[i].into(), 0.0);
+                        }
+                    }
+                },
+                RRCurveTo => {
+                    assert!(count % 6 == 0);
+                },
+                HHCurveTo => {
+                    if count % 4 == 0 {
+                    } else {
+                        assert!((count - 1) % 4 == 0);
+                    }
+                },
+                HVCurveTo => {
+                    if count % 8 == 0 {
+                    } else if (count - 1) % 8 == 0 {
+                    } else if (count - 4) % 8 == 0 {
+                    } else {
+                        assert!((count - 4 - 1) % 8 == 0);
+                    }
+                },
+                VHCurveTo => {
+                    if count % 8 == 0 {
+                    } else if (count - 1) % 8 == 0 {
+                    } else if (count - 4) % 8 == 0 {
+                    } else {
+                        assert!((count - 4 - 1) % 8 == 0);
+                    }
+                },
+                VVCurveTo => {
+                    if count % 4 == 0 {
+                    } else {
+                        assert!((count - 1) % 4 == 0);
+                    }
+                },
+                RCurveLine => {
+                    assert!(count >= 2 && (count - 2) % 6 == 0);
+                },
+                RLineCurve => {
+                    assert!(count >= 6 && (count - 6) % 2 == 0);
+                },
+                HStem | HStemHM | VStem | VStemHM | CntrMask | HintMask => {},
+                _ => unreachable!(),
+            }
+            match operator {
+                HMoveTo | VMoveTo | RMoveTo |
+                HStem | HStemHM | VStem | VStemHM |
+                CntrMask | HintMask => {
+                    clear = true;
+                },
+                _ => {},
+            }
+        }
+
+        Ok(Some(builder.into()))
     }
 }
--- src/glyph.rs
+++ src/glyph.rs
@@ -1,3 +1,44 @@
 /// A glyph.
 pub struct Glyph {
+    pub program: Vec<Operation>,
+}
+
+/// An operation.
+pub enum Operation {
+    /// Draw a line.
+    LineTo(f32, f32),
+    /// Move the cursor.
+    MoveTo(f32, f32),
+}
+
+pub struct Builder {
+    x: f32,
+    y: f32,
+    program: Vec<Operation>,
+}
+
+impl Builder {
+    #[inline]
+    pub fn new() -> Builder {
+        Builder { x: 0.0, y: 0.0, program: vec![] }
+    }
+
+    pub fn line_to(&mut self, x: f32, y: f32) {
+        self.x += x;
+        self.y += y;
+        self.program.push(Operation::LineTo(self.x, self.y));
+    }
+
+    pub fn move_to(&mut self, x: f32, y: f32) {
+        self.x += x;
+        self.y += y;
+        self.program.push(Operation::MoveTo(self.x, self.y));
+    }
+}
+
+impl From<Builder> for Glyph {
+    #[inline]
+    fn from(builder: Builder) -> Glyph {
+        Glyph { program: builder.program }
+    }
 }
