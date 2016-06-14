2a693a8bf9216c53c244e54b9efa28f2ace35ff2
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-14 10:44:28 +0200

    Add open_opentype to File

--- src/file.rs
+++ src/file.rs
@@ -14,13 +14,21 @@ pub struct File {
 impl File {
     /// Open a file.
     pub fn open<T: AsRef<Path>>(path: T) -> Result<Self> {
-        let path = path.as_ref();
-        let extension = some!(path.extension().and_then(|e| e.to_str()).map(|e| e.to_lowercase()),
-                              "unable to detect the file format");
+        let extension = path.as_ref()
+                            .extension()
+                            .and_then(|e| e.to_str())
+                            .map(|e| e.to_lowercase());
+        let extension = some!(extension, "unable to detect the file format");
         match &*extension {
-            "otc" | "otf" | "ttc" | "ttf" => Ok(File { fonts: try!(opentype::open(path)) }),
+            "otc" | "otf" | "ttc" | "ttf" => File::open_opentype(path),
             _ => raise!("encountered an unknown file format"),
         }
+    }
+
+    /// Open an OpenType file.
+    #[inline]
+    pub fn open_opentype<T: AsRef<Path>>(path: T) -> Result<Self> {
+        Ok(File { fonts: try!(opentype::open(path)) })
     }
 }
 
