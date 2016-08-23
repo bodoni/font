8ddff7a30b56e15c0b8449f04db2347e76230ed7
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-23 10:33:19 +0200

    Refactor a bit

--- src/format/opentype/mod.rs
+++ src/format/opentype/mod.rs
@@ -1,8 +1,18 @@
 use opentype;
+use postscript::compact::FontSet;
 use std::io::{Read, Seek};
 use std::rc::Rc;
+use truetype::{
+    CharMapping,
+    FontHeader,
+    GlyphData,
+    GlyphMapping,
+    HorizontalHeader,
+    HorizontalMetrics,
+    MaximumProfile,
+};
 
-use {Font, Number, Result};
+use {Case, Font, Number, Result};
 
 mod mapping;
 mod metrics;
@@ -14,55 +24,54 @@ use self::metrics::Metrics;
 use self::postscript::PostScript;
 use self::truetype::TrueType;
 
-pub fn read<T: Read + Seek>(tape: &mut T) -> Result<Vec<Font>> {
-    use postscript::compact::FontSet;
-    use truetype::{CharMapping, FontHeader, GlyphData, GlyphMapping,
-                   HorizontalHeader, HorizontalMetrics, MaximumProfile};
-
-    let file = try!(opentype::File::read(tape));
+pub fn read<T>(tape: &mut T) -> Result<Vec<Font>> where T: Read + Seek {
     let mut fonts = vec![];
-    for font in file.fonts {
-        let font_header = some!(try!(font.take::<_, FontHeader>(tape)),
-                                "cannot find the font header");
-        let horizontal_header = some!(try!(font.take::<_, HorizontalHeader>(tape)),
-                                      "cannot find the horizontal header");
-        let maximum_profile = some!(try!(font.take::<_, MaximumProfile>(tape)),
-                                    "cannot find the maximum profile");
-        let horizontal_metrics = some!(try!(font.take_given::<_, HorizontalMetrics>(
-            tape, (&horizontal_header, &maximum_profile))), "cannot find the horizontal metrics");
-        let char_mapping = some!(try!(font.take::<_, CharMapping>(tape)),
-                                 "cannot find the char-to-glyph mapping");
-        let metrics = Rc::new(try!(Metrics::new(horizontal_header, horizontal_metrics)));
-        let mapping = Rc::new(try!(Mapping::new(char_mapping)));
-        if let Some(font_set) = try!(font.take::<_, FontSet>(tape)) {
-            let font_set = Rc::new(font_set);
-            for id in 0..font_set.char_strings.len() {
-                fonts.push(Font {
-                    units_per_em: font_header.units_per_em as usize,
-                    ascender: Number::from(metrics.ascender),
-                    descender: Number::from(metrics.descender),
-                    case: Box::new(PostScript::new(id, font_set.clone(), metrics.clone(),
-                                                   mapping.clone())),
-                });
-            }
-            continue;
-        }
-        if let Some(glyph_mapping) = try!(font.take_given::<_, GlyphMapping>(
-            tape, (&font_header, &maximum_profile))) {
-
-            if let Some(glyph_data) = try!(font.take_given::<_, GlyphData>(tape, &glyph_mapping)) {
-                let glyph_data = Rc::new(glyph_data);
-                fonts.push(Font {
-                    units_per_em: font_header.units_per_em as usize,
-                    ascender: Number::from(metrics.ascender),
-                    descender: Number::from(metrics.descender),
-                    case: Box::new(TrueType::new(glyph_data.clone(), metrics.clone(),
-                                                 mapping.clone())),
-                });
-                continue;
-            }
-        }
-        raise!("failed to find glyph outlines");
+    for font in &try!(opentype::File::read(tape)).fonts {
+        try!(read_font(tape, &mut fonts, font));
     }
     Ok(fonts)
+}
+
+fn read_font<T>(tape: &mut T, fonts: &mut Vec<Font>, font: &opentype::Font) -> Result<()>
+    where T: Read + Seek
+{
+    let font_header = some!(try!(font.take::<_, FontHeader>(tape)), "cannot find the font header");
+    let horizontal_header = some!(try!(font.take::<_, HorizontalHeader>(tape)),
+                                  "cannot find the horizontal header");
+    let maximum_profile = some!(try!(font.take::<_, MaximumProfile>(tape)),
+                                "cannot find the maximum profile");
+    let horizontal_metrics = some!(try!(font.take_given::<_, HorizontalMetrics>(
+        tape, (&horizontal_header, &maximum_profile))), "cannot find the horizontal metrics");
+    let char_mapping = some!(try!(font.take::<_, CharMapping>(tape)),
+                             "cannot find the char-to-glyph mapping");
+    let metrics = Rc::new(try!(Metrics::new(horizontal_header, horizontal_metrics)));
+    let mapping = Rc::new(try!(Mapping::new(char_mapping)));
+    if let Some(font_set) = try!(font.take::<_, FontSet>(tape)) {
+        let font_set = Rc::new(font_set);
+        for id in 0..font_set.char_strings.len() {
+            let case = PostScript::new(id, font_set.clone(), metrics.clone(), mapping.clone());
+            fonts.push(new_font(&font_header, &metrics, Box::new(case)));
+        }
+        return Ok(());
+    }
+    if let Some(glyph_mapping) = try!(font.take_given::<_, GlyphMapping>(
+        tape, (&font_header, &maximum_profile))) {
+
+        if let Some(glyph_data) = try!(font.take_given::<_, GlyphData>(tape, &glyph_mapping)) {
+            let case = TrueType::new(Rc::new(glyph_data), metrics.clone(), mapping.clone());
+            fonts.push(new_font(&font_header, &metrics, Box::new(case)));
+            return Ok(());
+        }
+    }
+    raise!("failed to find glyph outlines");
+}
+
+#[inline]
+pub fn new_font(font_header: &FontHeader, metrics: &Metrics, case: Box<Case>) -> Font {
+    Font {
+        units_per_em: font_header.units_per_em as usize,
+        ascender: Number::from(metrics.ascender),
+        descender: Number::from(metrics.descender),
+        case: case,
+    }
 }
