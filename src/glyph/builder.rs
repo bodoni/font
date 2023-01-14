6da24bd00eae3bf30904e50f228be206284a8e71
Ivan Ukhov <ivan.ukhov@gmail.com>
2023-01-14 12:36:40 +0100

    Implement uniform scaling

--- src/glyph/builder.rs
+++ src/glyph/builder.rs
@@ -10,33 +10,6 @@ pub struct Builder {
 }
 
 impl Builder {
-    pub fn move_absolute<T: Into<Offset>>(&mut self, value: T) {
-        let last_position = match self.glyph.len() {
-            0 => Offset::default(),
-            count => self.glyph[count - 1].position,
-        };
-        let value = self.offset + value;
-        self.contour.offset = value - last_position;
-        self.contour.position = value;
-    }
-
-    pub fn move_relative<T: Into<Offset>>(&mut self, value: T) {
-        let value = value.into();
-        self.contour.offset += value;
-        self.contour.position += value;
-    }
-
-    pub fn move_control<T: Into<Offset>>(&mut self, a: T) {
-        let b = match self.contour.segments.get_mut(0) {
-            Some(&mut Segment::Quadratic(ref mut b, _)) => b,
-            Some(&mut Segment::Cubic(ref mut b, _, _)) => b,
-            _ => unreachable!(),
-        };
-        *b = a.into();
-    }
-}
-
-impl Builder {
     pub fn flush(&mut self) {
         use std::mem;
 
@@ -61,23 +34,60 @@ impl Builder {
         self.offset -= offset;
         result
     }
+
+    #[inline]
+    pub fn transform<T: Into<Offset>>(&self, value: T) -> Offset {
+        value.into() * self.scale
+    }
+}
+
+impl Builder {
+    pub fn move_absolute<T: Into<Offset>>(&mut self, a: T) {
+        let last_position = match self.glyph.len() {
+            0 => Offset::default(),
+            count => self.glyph[count - 1].position,
+        };
+        let a = self.offset + self.transform(a);
+        self.contour.offset = a - last_position;
+        self.contour.position = a;
+    }
+
+    pub fn move_relative<T: Into<Offset>>(&mut self, a: T) {
+        let a = self.transform(a);
+        self.contour.offset += a;
+        self.contour.position += a;
+    }
+
+    pub fn move_control<T: Into<Offset>>(&mut self, a: T) {
+        let a = self.transform(a);
+        let b = match self.contour.segments.get_mut(0) {
+            Some(&mut Segment::Quadratic(ref mut b, _)) => b,
+            Some(&mut Segment::Cubic(ref mut b, _, _)) => b,
+            _ => unreachable!(),
+        };
+        *b = a;
+    }
 }
 
 impl Builder {
     pub fn add_linear<T: Into<Offset>>(&mut self, a: T) {
-        self.contour.segments.push(Segment::Linear(a.into()));
+        self.contour
+            .segments
+            .push(Segment::Linear(self.transform(a)));
     }
 
     pub fn add_quadratic<T: Into<Offset>>(&mut self, a: T, b: T) {
         self.contour
             .segments
-            .push(Segment::Quadratic(a.into(), b.into()));
+            .push(Segment::Quadratic(self.transform(a), self.transform(b)));
     }
 
     pub fn add_cubic<T: Into<Offset>>(&mut self, a: T, b: T, c: T) {
-        self.contour
-            .segments
-            .push(Segment::Cubic(a.into(), b.into(), c.into()));
+        self.contour.segments.push(Segment::Cubic(
+            self.transform(a),
+            self.transform(b),
+            self.transform(c),
+        ));
     }
 }
 
