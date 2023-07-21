0f1189fc30eb25348478dd5e6a728a61becf9ec1
Ivan Ukhov <ivan.ukhov@gmail.com>
2023-07-21 08:43:08 +0200

    Update opentype

--- src/formats/opentype/characters.rs
+++ src/formats/opentype/characters.rs
@@ -7,30 +7,18 @@ use crate::Result;
 
 pub struct Characters(HashMap<u32, GlyphID>);
 
-macro_rules! remap(
-    ($source:expr) => ({
-        let source = $source;
-        let mut destination = HashMap::with_capacity(source.len());
-        for (&key, &value) in source.iter() {
-            destination.insert(key as u32, value);
-        }
-        destination
-    })
-);
-
 impl Characters {
     pub fn new(character_mapping: &CharacterMapping) -> Result<Self> {
-        if character_mapping.encodings.is_empty() {
-            raise!("found no character-to-glyph encoding");
+        for encoding in character_mapping.encodings.iter() {
+            match encoding {
+                Encoding::Format0(encoding) => return Ok(Self(encoding.mapping())),
+                Encoding::Format4(encoding) => return Ok(Self(encoding.mapping())),
+                Encoding::Format6(encoding) => return Ok(Self(encoding.mapping())),
+                Encoding::Format12(encoding) => return Ok(Self(encoding.mapping())),
+                _ => {}
+            }
         }
-        Ok(Self(match &character_mapping.encodings[0] {
-            Encoding::Format0(encoding) => remap!(encoding.mapping()),
-            Encoding::Format4(encoding) => remap!(encoding.mapping()),
-            Encoding::Format6(encoding) => remap!(encoding.mapping()),
-            Encoding::Format12(encoding) => encoding.mapping(),
-            Encoding::Format14(encoding) => encoding.mapping(),
-            _ => raise!("found an unknown character-to-glyph encoding"),
-        }))
+        raise!("found no known character-to-glyph encoding")
     }
 
     #[inline]
