e479e46784ce3b8c4bb43208b18268c3d953fa25
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-02-02 11:00:39 +0100

    Make a cosmetic adjustment

--- src/font.rs
+++ src/font.rs
@@ -12,7 +12,7 @@ macro_rules! implement {
     (
         $(
             $(#[$attribute:meta])*
-            fn $function:ident(&mut self $(, $argument_name:ident: $argument_type:ty)*) -> $type:ty;
+            fn $function:ident($($argument_name:ident: $argument_type:ty),*) -> $type:ty;
         )+
     ) => (
         /// A type that represents a font in a specific format.
@@ -37,21 +37,21 @@ macro_rules! implement {
 
 implement! {
     /// Return the axes.
-    fn axes(&mut self) -> Axes;
+    fn axes() -> Axes;
     /// Return the characters.
-    fn characters(&mut self) -> Characters;
+    fn characters() -> Characters;
     /// Return the features.
-    fn features(&mut self) -> Features;
+    fn features() -> Features;
     /// Return the metrics.
-    fn metrics(&mut self) -> Metrics;
+    fn metrics() -> Metrics;
     /// Return the names.
-    fn names(&mut self) -> Names;
+    fn names() -> Names;
     /// Return the palettes.
-    fn palettes(&mut self) -> Palettes;
+    fn palettes() -> Palettes;
     /// Return the tables.
-    fn tables(&mut self) -> Tables;
+    fn tables() -> Tables;
     /// Draw a character.
-    fn draw(&mut self, character: char) -> Option<Glyph>;
+    fn draw(character: char) -> Option<Glyph>;
 }
 
 pub fn read<T: typeface::tape::Read + 'static>(mut tape: T) -> Result<Vec<Font>> {
