9e9dd277a6c8994164992493da448e5dfecffc37
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-02-02 12:55:09 +0100

    Add a test for updating names

--- tests/names.rs
+++ tests/names.rs
@@ -1,41 +1,70 @@
 #[macro_use]
 mod support;
 
-use crate::support::{setup, Fixture};
+mod noto_color_emoji {
+    use font::opentype::truetype::tables::names::Names;
 
-#[test]
-fn noto_color_emoji() {
-    let font = &mut setup(Fixture::NotoColorEmoji)[0];
-    let table = ok!(font.names());
-    let names = table.borrow().iter().collect::<Vec<_>>();
-    let values = names
-        .iter()
-        .map(|(_, value)| ok!(value.as_deref()))
-        .collect::<Vec<_>>();
-    assert_eq!(
-        values,
-        &[
-            "Copyright 2022 Google Inc.",
-            "Noto Color Emoji",
-            "Regular",
-            "Noto Color Emoji",
-            "Noto Color Emoji",
-            "Version 2.042;GOOG;noto-emoji:20231129:7f49a00d523ae5f94e52fd9f9a39bac9cf65f958",
-            "NotoColorEmoji",
-            "Noto is a trademark of Google Inc.",
-            "Google, Inc.",
-            "Google, Inc.",
-            "Color emoji font using COLRv1.",
-            "https://github.com/googlefonts/noto-emoji",
-            "https://github.com/googlefonts/noto-emoji",
-            concat!(
-                "This Font Software is licensed under the SIL Open Font License, Version 1.1. ",
-                r#"This Font Software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR "#,
-                "CONDITIONS OF ANY KIND, either express or implied. See the SIL Open Font License ",
-                "for the specific language, permissions and limitations governing your use of ",
-                "this Font Software.",
-            ),
-            "http://scripts.sil.org/OFL",
-        ],
-    );
+    use crate::support::{setup, Fixture};
+
+    #[test]
+    fn read() {
+        let font = &mut setup(Fixture::NotoColorEmoji)[0];
+        let table = ok!(font.names());
+
+        let records = table.borrow().iter().collect::<Vec<_>>();
+        let values = records
+            .iter()
+            .map(|(_, value)| ok!(value.as_deref()))
+            .collect::<Vec<_>>();
+        assert_eq!(
+            values,
+            &[
+                "Copyright 2022 Google Inc.",
+                "Noto Color Emoji",
+                "Regular",
+                "Noto Color Emoji",
+                "Noto Color Emoji",
+                "Version 2.042;GOOG;noto-emoji:20231129:7f49a00d523ae5f94e52fd9f9a39bac9cf65f958",
+                "NotoColorEmoji",
+                "Noto is a trademark of Google Inc.",
+                "Google, Inc.",
+                "Google, Inc.",
+                "Color emoji font using COLRv1.",
+                "https://github.com/googlefonts/noto-emoji",
+                "https://github.com/googlefonts/noto-emoji",
+                concat!(
+                    "This Font Software is licensed under the SIL Open Font License, Version 1.1. ",
+                    r#"This Font Software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR "#,
+                    "CONDITIONS OF ANY KIND, either express or implied. See the SIL Open Font License ",
+                    "for the specific language, permissions and limitations governing your use of ",
+                    "this Font Software.",
+                ),
+                "http://scripts.sil.org/OFL",
+            ],
+        );
+    }
+
+    #[test]
+    fn write() {
+        let font = &mut setup(Fixture::NotoColorEmoji)[0];
+        let table = ok!(font.names());
+
+        let records = table
+            .borrow()
+            .iter()
+            .map(|(id, value)| (id, ok!(value)))
+            .collect::<Vec<_>>();
+        let language_tags = table
+            .borrow()
+            .language_tags()
+            .map(Option::unwrap)
+            .collect::<Vec<_>>();
+        let other = ok!(Names::from_iter(
+            records,
+            language_tags,
+            &mut Default::default(),
+        ));
+
+        *table.borrow_mut() = other;
+    }
 }
