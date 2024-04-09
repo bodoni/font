6edfd2df6292028fbfd41a84de6c6617b52e122c
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-04-09 09:15:02 +0200

    Add a test for File::read

--- src/file.rs
+++ src/file.rs
@@ -24,3 +24,21 @@ impl File {
 }
 
 dereference! { File::fonts => [crate::font::Font] }
+
+#[cfg(test)]
+mod tests {
+    macro_rules! ok(($result:expr) => ($result.unwrap()));
+
+    #[test]
+    fn cursor() {
+        use std::io::Read;
+
+        let path = "tests/fixtures/SourceSerifPro-Regular.otf";
+        let mut file = ok!(std::fs::File::open(path));
+        let mut data = Vec::new();
+        ok!(file.read_to_end(&mut data));
+
+        let data = std::io::Cursor::new(data);
+        let _ = ok!(super::File::read(data));
+    }
+}
